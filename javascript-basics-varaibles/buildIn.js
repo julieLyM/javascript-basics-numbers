7d96029f6636f678858d48e5365f13f57f004ac4
julielm77 <julielyminh@gmail.com>
2019-09-30 15:13:02 +0200

    build in method

---
+++ javascript-basics-varaibles/buildIn.js
@@ -0,0 +1,17 @@
+//Faites un commentaire
+
+alert("I know how to trigger an alert")
+
+const friend = "my friend";
+
+alert(friend);
+
+prompt("how old are you ?")
+
+const age = 20;
+
+alert("Vous avez" + " " + age + " " + "ans");
+
+console.log("Vous avez" + " " + age + " " + "ans");
+
+console.log((3 * 4) - 2)
