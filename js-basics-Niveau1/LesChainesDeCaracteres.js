69dac9f9627ff24a36e5e57de207222f77c728fc
julielm77 <julielyminh@gmail.com>
2019-09-30 14:45:26 +0200

    chaines de carac

---
+++ js-basics-Niveau1/LesChainesDeCaracteres.js
@@ -0,0 +1,41 @@
+// Niveau 1 - Les chaines de caractères
+
+"Hello world"
+"Power!"
+"Is it a question ?"
+"`Best answer: learn it!`"
+
+
+"J'adore le javascript"
+"J'imaginais ça autrement"
+"C'est drôle"
+"`N'oubliez pas le \`"
+"`N'oubliez pas d'en mettre deux \`"
+
+"J'ai un numéro fétiche".length
+22
+
+"Mon numéro préféré est le 42".length
+28
+
+"J'ai un numéro fétiche" + "Mon numéro préféré est le 42"
+
+"21 + 21" + " = 42"
+
+"Pierre a " + 10 + " amis sur Facebook"
+"Pierre a 10 amis sur Facebook"
+
+"Paul a " + "abcdefghijklmnopqrstuvwxyz".length * 10 + " amis sur Facebook"
+"Paul a 260 amis sur Facebook"
+
+"Jacques en a " + (32 - 32)
+"Jacques en a 0"
+
+"Jacques".length % "Pierre".length
+1
+
+"`Jacques\`".length % "Pierre".length
+3
+
+  ("Pierre" + "Paul").length % "Jacques".length
+3
