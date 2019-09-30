54432b37ed93d39b21b23edb33c61aca89c68e19
julielm77 <julielyminh@gmail.com>
2019-09-30 14:57:43 +0200

    variables

---
+++ js-basics-Niveau1/LesVariables.js
@@ -0,0 +1,12 @@
+const nbFriend = 42;
+
+const liarCoef = 10;
+
+console.log(nbFriend);
+
+console.log(liarCoef);
+
+isALiar = true;
+isNotALiar = false;
+console.log('john is isAliar or isNotAliar');
+
