0643ef3c6661390efbbfbe157e715510514a2ebf
julielm77 <julielyminh@gmail.com>
2019-09-30 15:57:11 +0200

    external script

---
+++ javascript-basics-varaibles/javascript-external-script/userQueries.js
@@ -0,0 +1,6 @@
+//inform to us informations to the user
+const name = prompt("quel est votre nom ?");
+const firstName = prompt(" quel est votre prénom ?");
+const age = prompt("quel est votre age ?");
+
+console.log(`Bonjour ${name} ${firstName}. Vous avez ${age}.`)
